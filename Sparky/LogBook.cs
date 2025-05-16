5799a7921c29adc93d098bd6db6d411548e588e9
arminjho <armin.muratspahic@htecgroup.com>
2025-05-16 15:12:49 +0200

    Add project files.

---
+++ Sparky/LogBook.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+
+    public interface ILogBook
+    {
+        void Message(string message);   
+        bool LogToDb(string message);
+        bool LogBalanceAfterWithdrawal(int balanceAfterWithdrawal);
+        string MessageWithReturnStr(string message);
+    }
+    public class LogBook : ILogBook
+    {
+        public void Message(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public string MessageWithReturnStr(string message)
+        {
+            Console.WriteLine(message);
+            return message.ToLower();
+        }
+
+        public bool LogBalanceAfterWithdrawal(int balanceAfterWithdrawal)
+        {
+            if (balanceAfterWithdrawal >= 0)
+            {
+                Console.WriteLine("Success");
+                return true;
+            }
+            Console.WriteLine("failure");
+            return false;
+        }
+
+
+        public bool LogToDb(string message)
+        {
+            Console.WriteLine(message);
+            return true;
+        }
+    }
+   
+}
---
+++ SparkyNUnitTest/ProductNUnitTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using Sparky;
+
+
+namespace SparkyNUnitTest
+{
+    [TestFixture]
+    public class ProductNUnitTests
+    {
+        [Test]
+        public void GetProductPrice_PlatinumCustomer_ReturnPriceWith20Discount()
+        {
+            Product product = new() { Price = 50 };
+
+            var result = product.GetPrice(new Customer() { IsPlatinum = true });
+             
+            Assert.That(result, Is.EqualTo(40));
+        }
+
+        [Test]
+        public void GetProductPriceMOQAbuse_PlatinumCustomer_ReturnPriceWith20Discount()
+        {
+            var customer=new Mock<ICustomer>();
+            customer.Setup(u=>u.IsPlatinum).Returns(true);
+
+            Product product = new() { Price = 50 };
+
+            var result = product.GetPrice(customer.Object);
+
+            Assert.That(result, Is.EqualTo(40));
+        }
+
+
+    }
+}
